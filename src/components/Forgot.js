7c595c451820fa6fae90f6c38576047ad3b0b229
HilelBenhamou <hilelbenhamou@hotmail.fr>
2020-07-20 11:47:26 +0300

    lastCommit

--- src/components/Forgot.js
+++ src/components/Forgot.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import ReCAPTCHA from "react-google-recaptcha";
 
 function Forgot() {
+  //
   const input_valid = {
     border:"3px solid green",
     borderRadius: "5px",
@@ -20,24 +21,25 @@ function Forgot() {
     borderRadius: "5px",
     padding:"5px",
   }
+  //
   const image = require('./images.png');
-
+  //
   const [email, setEmail] = useState('')
   function handleChange(e){
     console.log(e.target.value)
     setEmail(e.target.value)
   }
-  function validateEmail(email) 
-    {
-      var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  function validateEmail(email) {
+    var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
 
-      return re.test(email);
-    }
+    return re.test(email);
+  }
+  //
   const [password, setPassword] = useState('');
   function handlePsw(e){
     setPassword(e.target.value);
   }
-  
+  //
   function onChangeCaptcha(value) {
     console.log("Captcha value:", value);
   }
